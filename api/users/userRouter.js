0d51925b719cd5ff14718f8a494ac899b9c1f4ea
chrisbonifacio <christopher.bonifacio@gmail.com>
2019-10-21 20:05:32 -0400

    moved files

--- api/users/userRouter.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const router = express.Router();
-
-router.get("/users", async (req, res) => {
-  res.status(200).json({ message: "user router running" });
-});
-
-module.exports = router;
