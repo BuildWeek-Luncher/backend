80dd2e8767d2cfd6ed0d772b7bb1f3a2720fe970
chrisbonifacio <christopher.bonifacio@gmail.com>
2019-10-22 13:37:27 -0400

    create new seeds for schools tables

--- data/seeds/002-schools.js
+++ data/seeds/002-schools.js
@@ -14,6 +14,26 @@ exports.seed = function(knex) {
           zipcode: "10463",
           funds_needed: 50000,
           funds_raised: 0
+        },
+        {
+          admin_id: 3,
+          school_name: "Bronx High School of Science",
+          address: "123 Heroku St",
+          city: "Bronx",
+          state: "NY",
+          zipcode: "10463",
+          funds_needed: 90000,
+          funds_raised: 0
+        },
+        {
+          admin_id: 2,
+          school_name: "John F. Kennedy High School",
+          address: "123 Heroku St",
+          city: "Bronx",
+          state: "NY",
+          zipcode: "10463",
+          funds_needed: 30000,
+          funds_raised: 0
         }
       ]);
     });
